11c01bb694fc5d0e2ad3f33bc308de5c52bd63d9
tingSHI <ting.shi2015@gmail.com>
2024-04-22 12:59:55 +0200

    - add .idea in .gitignore - add  test & function method for first 4 questions

--- src/main/java/PasswordValidation.java
+++ src/main/java/PasswordValidation.java
@@ -1,5 +1,29 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class PasswordValidation {
     public static void main(String[] args) {
         System.out.println("Hello World from Ting 2024.4.22");
     }
+
+    public static boolean isPassword8CharactersLongEnough(String password) {
+        return password.length() >= 8;
+    }
+
+    public static boolean isPasswordContainsDigits(String password){
+    //    return  password.matches(".*[0-9].*");
+        return password.matches(".*\\d.*");       //----- not '//d'
+        }
+
+    public static boolean isPasswordContainsBothCases(String password){
+        return password.matches(".*[A-Z].*") && password.matches(".*[a-z].*");
+    }
+
+    public static boolean isPasswordCommon(String password){
+        List<String> commonPasswords = Arrays.asList("password", "Password1", "123456", "12345", "Mary1989");
+        return commonPasswords.contains(password);
+    }
+
+
+
 }
---
+++ src/test/java/testPasswordValidation.java
@@ -0,0 +1,183 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class testPasswordValidation {
+
+    //Tests for the  1st. Method --------------------------
+    @Test
+    public void testWhenPasswordLengthIs9ThenReturnTrue() {
+        //GIVEN
+        String password = "123456789";
+
+        //WHEN
+        boolean actual = PasswordValidation.isPassword8CharactersLongEnough(password);
+
+        //THEN
+        boolean expect = true;
+        assertEquals(expect, actual, "test failed for testWhenPasswordLengthIs9");
+    }
+
+    @Test
+    public void testWhenPasswordLengthIs8ThenReturnTrue() {
+        //GIVEN
+        String password = "12345678";
+
+        //WHEN
+        boolean actual = PasswordValidation.isPassword8CharactersLongEnough(password);
+
+        //THEN
+        boolean expect = true;
+        assertEquals(expect, actual, "test failed for testWhenPasswordLengthIs8");
+    }
+
+    @Test
+    public void testWhenPasswordLengthIs7ThenReturnFalse() {
+        //GIVEN
+        String password = "1234567";
+
+        //WHEN
+        boolean actual = PasswordValidation.isPassword8CharactersLongEnough(password);
+
+        //THEN
+        boolean expect = false;
+        assertEquals(expect, actual, "test failed for testWhenPasswordLengthIs7");
+    }
+
+    @Test
+    public void testWhenPasswordLengthIsNullThenReturnFalse() {   // "" is ?Null or Empty?
+        //GIVEN
+        String password = "";
+
+        //WHEN
+        boolean actual = PasswordValidation.isPassword8CharactersLongEnough(password);
+
+        //THEN
+        boolean expect = false;
+        assertEquals(expect, actual, "test failed for testWhenPasswordLengthIsNull");
+    }
+
+
+    //Tests for the  2nd. Method --------------------------
+    @Test
+    public void testWhenPasswordContains1DigitsAtBeginThenReturnTrue() {
+        //GIVEN
+        String password = "1AbcDefG";     //also passt for other test?!
+
+        //WHEN
+        boolean actual = PasswordValidation.isPasswordContainsDigits(password);
+
+        //THEN
+        boolean expect = true;
+        assertEquals(expect, actual, "test failed for testWhenPasswordContains1DigitsAtBegin");
+    }
+
+    @Test
+    public void testWhenPasswordContains1DigitsAtTheEndThenReturnTrue() {
+        //GIVEN
+        String password = "AbcDefG1";     //also passt for other test?!
+
+        //WHEN
+        boolean actual = PasswordValidation.isPasswordContainsDigits(password);
+
+        //THEN
+        boolean expect = true;
+        assertEquals(expect, actual, "test failed for testWhenPasswordContains1DigitsAtTheEnd");
+    }
+
+    @Test
+    public void testWhenPasswordContainsNoDigitThenReturnFalse() {
+        //GIVEN
+        String password = "AbcDefG";
+
+        //WHEN
+        boolean actual = PasswordValidation.isPasswordContainsDigits(password);
+
+        //THEN
+        boolean expect = false;
+        assertEquals(expect, actual, "test failed for testWhenPasswordContainsNoDigit");
+    }
+
+
+    //Tests for the  3rd. Method --------------------------
+    @Test
+    public void testWhenPasswordContainsBothCases_2a4684Z5_ThenReturnTrue() {
+        //GIVEN
+        String password = "2a4684Z5";
+
+        //WHEN
+        boolean actual = PasswordValidation.isPasswordContainsBothCases(password);
+
+        //THEN
+        boolean expect = true;
+        assertEquals(expect, actual, "test failed for testWhenPasswordContainsBothCase_2a4684Z5");
+    }
+
+    @Test
+    public void testWhenPasswordContainsOnlyLowerCases_2a46845_ThenReturnTrue() {
+        //GIVEN
+        String password = "2a46845";
+
+        //WHEN
+        boolean actual = PasswordValidation.isPasswordContainsBothCases(password);
+
+        //THEN
+        boolean expect = false;
+        assertEquals(expect, actual, "test failed for testWhenPasswordContainsOnlyUpperCases_2a46845");
+    }
+
+    @Test
+    public void testWhenPasswordContainsOnlyUpperCases_2a46845_ThenReturnTrue() {
+        //GIVEN
+        String password = "246T845";
+
+        //WHEN
+        boolean actual = PasswordValidation.isPasswordContainsBothCases(password);
+
+        //THEN
+        boolean expect = false;
+        assertEquals(expect, actual, "test failed for testWhenPasswordContainsOnlyUpperCases_246T845");
+    }
+
+
+    //Tests for the  4th. Method --------------------------
+    @Test
+    public void testWhenPassword_Mary123J_ThenReturnFalse() {
+        //GIVEN
+        String password = "Mary123J";
+
+        //WHEN
+        boolean actual = PasswordValidation.isPasswordCommon(password);
+
+        //THEN
+        boolean expect = false;
+        assertEquals(expect, actual, "test failed for testWhenPassword_Mary123J_ThenReturnFalse");
+    }
+
+    @Test
+    public void testWhenPassword_Password1_ThenReturnTrue() {
+        //GIVEN
+        String password = "Password1";
+
+        //WHEN
+        boolean actual = PasswordValidation.isPasswordCommon(password);
+
+        //THEN
+        boolean expect = true;
+        assertEquals(expect, actual, "test failed for testWhenPassword_Password1_ThenReturnTrue");
+    }
+
+    @Test
+    public void testWhenPassword_Aa345678_ThenReturnFalse() {
+        //GIVEN
+        String password = "Aa345678";
+
+        //WHEN
+        boolean actual = PasswordValidation.isPasswordCommon(password);
+
+        //THEN
+        boolean expect = false;
+        assertEquals(expect, actual, "test failed for testWhenPassword_Aa345678_ThenReturnFalse");
+    }
+
+}
